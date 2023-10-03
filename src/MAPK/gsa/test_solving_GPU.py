f0190bc28efbbbea8dc0cb5257e217a74dbb2865
Nathaniel Linden <natejlinden@gmail.com>
2023-10-03 20:27:45 +0000

    updated issues and specify GPU on Martini

--- src/MAPK/gsa/test_solving_GPU.py
+++ src/MAPK/gsa/test_solving_GPU.py
@@ -1,5 +1,6 @@
 from os import environ
 environ['OMP_NUM_THREADS'] = '1'
+environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import diffrax
 import matplotlib.pyplot as plt
@@ -17,8 +18,9 @@ from hornberg_2005 import *
 
 jax.config.update("jax_enable_x64", True)
 
-n_devices = jax.local_device_count() 
-print('Using {} jax devices'.format(n_devices))
+#n_devices = jax.local_device_count() 
+#print('Using {} jax devices'.format(n_devices))
+print('Device memory is {}'.format(jax.devices()[0].memory_stats()))
 
 # function to solve the model and get a trajectory
 @jax.jit
@@ -56,19 +58,20 @@ psolve_traj = jax.pmap(vsolve_traj, in_axes=(None, None, 0, None, None))
 HB_2005 = hornberg_2005(transient=False)
 pdict, plist = HB_2005.get_nominal_params()
 y0_dict, y0 = HB_2005.get_initial_conditions()
+HB_2005_ode = diffrax.ODETerm(HB_2005)
 
 # compile + solve model
 times = jnp.linspace(0, 6000, 501)
 start = time.time()
-sol =  solve_traj(HB_2005, y0, plist, 6000, times)
+sol =  solve_traj(HB_2005_ode, y0, plist, 6000, times)
 end = time.time()
 print('Compile + solves took {} seconds'.format(end-start))
 
 # repeat solve 10 times
-nsolves = 10
+nsolves = 100
 plist = jnp.repeat(jnp.array([plist]), nsolves, axis=0)
 times = jnp.linspace(0, 6000, 501)
 start = time.time()
-sol =  vsolve_traj(HB_2005, y0, plist, 6000, times)
+sol =  vsolve_traj(HB_2005_ode, y0, plist, 6000, times)
 end = time.time()
 print('VMAP solves took {} seconds'.format(end-start))
