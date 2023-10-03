f3896ddfea19de784ece5f40d5656d5ba0d50596
Nathaniel Linden <nlinden@ucsd.edu>
2023-10-03 13:08:15 -0700

    parrallelize on a GPU with VMAP

--- src/MAPK/gsa/test_solving_GPU.py
+++ src/MAPK/gsa/test_solving_GPU.py
@@ -1,12 +1,12 @@
 from os import environ
 environ['OMP_NUM_THREADS'] = '1'
 
-import numpy as np
 import diffrax
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import jax
+import jax.numpy as jnp
 import equinox as eqx
 import sys
 from scipy.stats import mode
@@ -20,30 +20,37 @@ jax.config.update("jax_enable_x64", True)
 n_devices = jax.local_device_count() 
 print('Using {} jax devices'.format(n_devices))
 
-
-# function to solve
-def simulate_model_trajectory(model_instance, y0, params, t1=300, dt0=1e-3, n_out=1000):
+# function to solve the model and get a trajectory
+@jax.jit
+def solve_traj(model_dfrx_ode, y0, params, t1, times):
     """ simulates a model over the specified time interval and returns the 
-    trajectory of the model state variables."""
-    ode_term = diffrax.ODETerm(model_instance)
+    calculated steady-state values.
+    Returns an array of shape (n_species, 1) """
+    dt0=1e-3
     solver = diffrax.Kvaerno5()
-    stepsize_controller = diffrax.PIDController(rtol=1e-6, atol=1e-6)
+    stepsize_controller=diffrax.PIDController(rtol=1e-6, atol=1e-6)
     t0 = 0.0
     dt0 = 1e-3
-    times = np.linspace(t0, t1, n_out)
     saveat = saveat=diffrax.SaveAt(ts=times)
 
     sol = diffrax.diffeqsolve(
-        ode_term, 
+        model_dfrx_ode, 
         solver, 
         t0, t1, dt0, 
         y0, 
-        saveat=saveat, stepsize_controller=stepsize_controller,
-        args=tuple(params),
+        saveat=saveat,
+        stepsize_controller=stepsize_controller,
+        args=tuple(list(params)),
         max_steps=60000,
         throw=False,)
     
-    return sol
+    return jnp.array(sol.ys)
+
+# vmap it over the parameters
+# assume that the first dimension of params is the number of samples
+# and the second dimension is the number of parameters
+vsolve_traj = jax.vmap(solve_traj, in_axes=(None, None, 0, None, None))
+psolve_traj = jax.pmap(vsolve_traj, in_axes=(None, None, 0, None, None))
 
 # import model
 HB_2005 = hornberg_2005(transient=False)
@@ -51,18 +58,17 @@ pdict, plist = HB_2005.get_nominal_params()
 y0_dict, y0 = HB_2005.get_initial_conditions()
 
 # compile + solve model
+times = jnp.linspace(0, 6000, 501)
 start = time.time()
-sol = simulate_model_trajectory(HB_2005, y0, plist, t1=6000)
+sol =  solve_traj(HB_2005, y0, plist, 6000, times)
 end = time.time()
 print('Compile + solves took {} seconds'.format(end-start))
 
 # repeat solve 10 times
-n_solves = 10
-times = []
-for i in range(n_solves):
-    start = time.time()
-    sol = simulate_model_trajectory(HB_2005, y0, plist, t1=6000)
-    end = time.time()
-    times.append(end-start)
-
-print('Average solve time: {} seconds'.format(np.mean(times)))
+nsolves = 10
+plist = jnp.repeat(jnp.array([plist]), nsolves, axis=0)
+times = jnp.linspace(0, 6000, 501)
+start = time.time()
+sol =  vsolve_traj(HB_2005, y0, plist, 6000, times)
+end = time.time()
+print('VMAP solves took {} seconds'.format(end-start))
