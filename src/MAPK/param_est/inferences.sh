33c47c14ec8af4821b28a8693b2829e76a6a89c0
Nathaniel Linden <nlinden@ucsd.edu>
2023-10-20 15:31:54 -0700

    change conversion factor for BF_2000

--- src/MAPK/param_est/inferences.sh
+++ src/MAPK/param_est/inferences.sh
@@ -6,7 +6,7 @@
 python inference_process.py -model kholodenko_2000 -free_params K8,v10,v9,K7,K9,KI,MAPK_total,K10 -data_file ../../../results/MAPK/HF_96_synthetic_data.csv -nsamples 4000 -ncores 8 -savedir ../../../results/MAPK/param_est/ -t1 4800 -input_state Input -ERK_states MAPK_PP
 
 # Birghtman and Fell 2000
-python inference_process.py -model brightman_fell_2000 -free_params kn14,K_24,kn16,V_26,V_24,kn12,k15,k_13,K_25,K_26,k17 -data_file ../../../results/MAPK/HF_96_synthetic_data.csv -nsamples 4000 -ncores 8 -savedir ../../../results/MAPK/param_est/ -t1 360 -input_state L -EGF_conversion_factor 602214 -ERK_states ERKPP
+python inference_process.py -model brightman_fell_2000 -free_params kn14,K_24,kn16,V_26,V_24,kn12,k15,k_13,K_25,K_26,k17 -data_file ../../../results/MAPK/HF_96_synthetic_data.csv -nsamples 4000 -ncores 8 -savedir ../../../results/MAPK/param_est/ -t1 360 -input_state L -EGF_conversion_factor 1e-7 -ERK_states ERKPP
 
 # LEVCHENKO 2000
 python inference_process.py -model levchenko_2000 -free_params kOff1,kOn1,d8,RAFPase,k2,k6,k10,MEKPase,a2,kOn2,k3,d7,d9,d6,a10,kOff3,a9,a8,a3,a5,d3,d5,a6,k7,kOff4,d2,d10,a1,a4,k9,k5,k8,k4,d1,kOff2,a7,MAPKPase,d4,k1 -data_file ../../../results/MAPK/HF_96_synthetic_data.csv -nsamples 4000 -ncores 8 -savedir ../../../results/MAPK/param_est/ -t1 1000 -input_state RAFact -EGF_conversion_factor 0.001 -ERK_states MAPKstarstar
