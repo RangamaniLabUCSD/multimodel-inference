be397dc1cd53fb08e893c27baf667303216e0853
Nathaniel Linden <nlinden@ucsd.edu>
2024-03-20 16:41:49 -0700

    remove old models

--- src/MAPK/gsa/gsa_sampling.sh
+++ src/MAPK/gsa/gsa_sampling.sh
@@ -6,14 +6,8 @@ XLA_PYTHON_CLIENT_PREALLOCATE=false python gsa_sample.py huang_ferrell_1996 -ana
 # Kholodenko 2000
 XLA_PYTHON_CLIENT_PREALLOCATE=false python gsa_sample.py kholodenko_2000 -analyze_params K8,v10,v9,K7,K9,KI,MAPK_total,K10 -n_samples 256 -savedir ../../../results/MAPK/gsa/ -max_time 2400 -input_state Input --full_trajectory -ERK_state_indices -1 -lower 1e-2 -upper 1e2
 
-# # Birghtman and Fell 2000
-# XLA_PYTHON_CLIENT_PREALLOCATE=false python gsa_sample.py brightman_fell_2000 -analyze_params kn14,K_24,kn16,V_26,kn1,k3,V_24,kn12,k15,k_13,kn7,kn11,K_25,k2_4,K_26,k17,K_23,DT -n_samples 256 -savedir ../../../results/MAPK/gsa/ -max_time 360 -input_state L -input 60221 --full_trajectory -lower 1e-2 -upper 1e2
-
 # LEVCHENKO 2000
 CUDA_VISIBLE_DEVICES=0 XLA_PYTHON_CLIENT_PREALLOCATE=false python gsa_sample.py levchenko_2000 -analyze_params kOff1,a2,kOn1,k10,RAFPase,k4,k6,kOff4,kOn2,total_scaffold,d7,d9,d3,a3,a10,kOff3,k8,k5,a9,a8,a5,d8,d5,a6,k7,d2,a7,d10,a1,a4,k9,d6,k2,d1,k3,MEKPase,kOff2,MAPKPase,d4,k1 -n_samples 256 -savedir ../../../results/MAPK/gsa/ -max_time 6000 -input_state RAFact -input 0.0001 -ERK_state_indices 16 --full_trajectory -lower 1e-2 -upper 1e2
-
-# Hatakeyama 2003
-CUDA_VISIBLE_DEVICES=0 XLA_PYTHON_CLIENT_PREALLOCATE=false python gsa_sample.py hatakeyama_2003 -analyze_params k21,k20,kb29,kf9,kb24,kb3,kb23,kf25,kb1,kb2,kf24,kb7,kf8,k19,kf34,k22,kb5,kf6,kf3,kb6 -n_samples 256 -savedir ../../../results/MAPK/gsa/ -max_time 10800 -input_state HRG --full_trajectory -ERK_state_indices 32 -lower 1e-2 -upper 1e2
 
 # Hornberg 2005
 CUDA_VISIBLE_DEVICES=0 XLA_PYTHON_CLIENT_PREALLOCATE=false python gsa_sample.py hornberg_2005 -analyze_params kd1,k2,kd2,k3,kd3,k4,kd4,k5,kd5,k6,kd6,k8,kd8,k10b,kd10,k13,kd13,k15,kd15,k16,kd63,k17,kd17,k18,kd18,k19,kd19,k20,kd20,k21,kd21,k22,kd22,k23,kd23,kd24,k25,kd25,k28,kd28,k29,kd29,k32,kd32,k33,kd33,k34,kd34,k35,kd35,k36,kd36,k37,kd37,k40,kd40,k41,kd41,k42,kd42,k43,kd43,k44,kd52,k45,kd45,k47,kd47,k48,kd48,k49,kd49,k50,kd50,k52,kd44,k53,kd53,k55,kd55,k56,kd56,k57,kd57,k58,kd58,k60,kd60,k61,kd61,k126,kd126,k127,kd127 -n_samples 30 -savedir ../../../results/MAPK/gsa/ -max_time 12000 -input_state c1 -input 1e-9 --full_trajectory -ERK_state_indices 58,82 -lower 1e-2 -upper 1e2
