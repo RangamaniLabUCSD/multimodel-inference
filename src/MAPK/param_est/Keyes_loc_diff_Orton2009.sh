ce704f1f8ac9fe5b335eee86f52145d4199bdb1c
Nathaniel Linden <nlinden@ucsd.edu>
2025-02-06 18:24:16 -0800

    csv file and bash script to run exhaustive param est for o 2009

---
+++ src/MAPK/param_est/Keyes_loc_diff_Orton2009.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the CSV file path
+CSV_FILE="../../../mmi_old_code/loc-dff.csv"
+
+# number of models to process
+n_models=$(wc -l < ../../../mmi_old_code/loc-dff.csv)
+
+# Initialize counter
+counter=0
+
+# Read the CSV file line by line, skipping the header
+tail -n +1 "$CSV_FILE" | while IFS='.' read -r loc_diff_case diff_params _; do
+    ((counter++))  # Increment counter
+    savedir="../../../results/MAPK/param_est/Keyes_2020_data_locDiffs/orton_2009/$loc_diff_case"
+    diff_params=$(echo "$diff_params" | tr -d '"') # remove leading and trailing quotes
+
+    echo "Processing $loc_diff_case with diff_params: $diff_params"
+    echo "($counter of $n_models)"
+    
+
+    python inference_process_location_diff_traj.py -model orton_2009 -free_params km_Erk_Activation,k1_C3G_Deactivation,km_Erk_Deactivation,k1_P90Rsk_Deactivation,k1_Sos_Deactivation -diff_params "$diff_params" -Rap1_state Rap1Inactive -savedir "$savedir" -input_state EGF -EGF_conversion_factor 602214 -ERK_states ErkActive -prior_family "[['LogNormal(mu=13.822823751258499)',['sigma']],['LogNormal(mu=0.9162907318741551)',['sigma']],['LogNormal(mu=15.067270166119108)',['sigma']],['LogNormal(mu=-5.298317366548036)',['sigma']],['LogNormal(mu=0.9162907318741551)',['sigma']]]" -time_conversion_factor 60 -nsamples 500 -ncores 4 -nchains 4
+done
