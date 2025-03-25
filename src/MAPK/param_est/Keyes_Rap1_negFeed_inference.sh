540a0d385eebba18f432525cf7eaca74d5801a23
Nathaniel Linden <nlinden@ucsd.edu>
2025-03-25 16:46:04 -0700

    some changes

--- src/MAPK/param_est/Keyes_Rap1_negFeed_inference.sh
+++ src/MAPK/param_est/Keyes_Rap1_negFeed_inference.sh
@@ -20,6 +20,8 @@ python inference_process_location_diff_traj.py -model orton_2009 -pymc_model Ort
 # Shin 2014
 ################################################################
 # all diff
+python inference_process_location_diff_traj.py -model shin_2014_Rap1 -free_params kc47,kc43,kd39,kc45,ERK_tot,ki39,kc41 -diff_params kc47,kc43,kd39,kc45,ERK_tot,ki39,kc41 -Rap1_state Rap1 -ERK_states pp_ERK -savedir ../../../results/MAPK/param_est/Keyes_2020_data_locDiffs/shin_2014/all_diff/all_diff_ -input_state EGF -EGF_conversion_factor 0.001 -ERK_states pp_ERK -prior_family "[['LogNormal(mu=-0.9403273097023963)',['sigma']],['LogNormal(mu=1.539659017826184)',['sigma']],['LogNormal(mu=-1.822631132895142)',['sigma']],['LogNormal(mu=-2.677715001306137)',['sigma']],['LogNormal(mu=-1.487220279709851)',['sigma']],['LogNormal(mu=-7.1670442769327005)',['sigma']],['LogNormal(mu=3.9265174515785985)',['sigma']]]" -time_conversion_factor 60 -nsamples 500 -ncores 4 -nchains 4
+
 python inference_process_location_diff_traj.py -model shin_2014_Rap1 -pymc_model all_diff -free_params kc47,kc43,kd39,kc45,ERK_tot,ki39,kc41 -Rap1_state Rap1 -ERK_states pp_ERK -savedir ../../../results/MAPK/param_est/Keyes_2020_data_locDiffs/shin_2014/all_diff/all_diff_ -input_state EGF -EGF_conversion_factor 0.001 -ERK_states pp_ERK -prior_family "[['LogNormal(mu=-0.9403273097023963)',['sigma']],['LogNormal(mu=1.539659017826184)',['sigma']],['LogNormal(mu=-1.822631132895142)',['sigma']],['LogNormal(mu=-2.677715001306137)',['sigma']],['LogNormal(mu=-1.487220279709851)',['sigma']],['LogNormal(mu=-7.1670442769327005)',['sigma']],['LogNormal(mu=3.9265174515785985)',['sigma']]]" -time_conversion_factor 60 -nsamples 500 -ncores 4 -nchains 4
 
 # Rap1 diff
